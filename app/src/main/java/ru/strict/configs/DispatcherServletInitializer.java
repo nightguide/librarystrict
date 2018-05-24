8b14b0728784374f37169948ee3cbb9e5bd7dc56
Konstantin <strictkod1899@yandex.ru>
2018-05-24 18:19:42 +0700

    feature (spring): added spring-config

---
+++ app/src/main/java/ru/strict/configs/DispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package ru.strict.configs;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { ru.strict.configs.ViewConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
---
+++ app/src/main/java/ru/strict/configs/IoC.java
@@ -0,0 +1,11 @@
+package ru.strict.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.strict.*")
+public class IoC {
+
+}
