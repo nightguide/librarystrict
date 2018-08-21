e9ea32d3ab4c2ec335c0f1f1e2eb6607dc466910
Konstantin <konstantin.kastirin@actonica.ru>
2018-08-21 11:14:42 +0800

    chore (db): updated an sql-script for create tables

--- _sources/sql/1. create tables.sql
+++ _sources/sql/1. create tables.sql
@@ -1,20 +1,5 @@
 /* PostgreSQL */
 
-CREATE TABLE public.publishing(
-id uuid NOT NULL,
-name VARCHAR(255) NOT NULL,
-CONSTRAINT pk_publishing PRIMARY KEY (id)
-);
-
-CREATE TABLE public.genre(
-id UUID NOT NULL,
-name VARCHAR(255) NOT NULL,
-parent_id UUID,
-CONSTRAINT pk_genre PRIMARY KEY (id),
-CONSTRAINT fk_genre_genre_id FOREIGN KEY (parent_id) REFERENCES genre(id) ON UPDATE CASCADE ON DELETE CASCADE,
-CONSTRAINT uq_genre_name UNIQUE (name)
-);
-
 CREATE TABLE public.country(
 id UUID NOT NULL,
 caption TEXT NOT NULL,
@@ -28,16 +13,6 @@ country_id UUID NOT NULL,
 CONSTRAINT pk_city PRIMARY KEY (id),
 CONSTRAINT uq_city_caption UNIQUE (caption, country_id),
 CONSTRAINT fk_city_country_id FOREIGN KEY (country_id) REFERENCES country(id));
-
-CREATE TABLE public.author(
-id UUID NOT NULL,
-name VARCHAR(255) NOT NULL,
-lastname VARCHAR(255) NOT NULL,
-middlename VARCHAR(255),
-country_id UUID NOT NULL,
-CONSTRAINT pk_id PRIMARY KEY (id),
-CONSTRAINT fk_author_country_id FOREIGN KEY (country_id) REFERENCES country(id) ON UPDATE CASCADE ON DELETE CASCADE
-);
 
 CREATE TABLE public.roleuser(
 id UUID NOT NULL,
@@ -96,6 +71,31 @@ CONSTRAINT pk_token PRIMARY KEY (id),
 CONSTRAINT fk_token_userx_id FOREIGN KEY (userx_id) REFERENCES userx(id) ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT fk_token_roleuser_id FOREIGN KEY (roleuser_id) REFERENCES roleuser(id) ON UPDATE CASCADE ON DELETE CASCADE);
 
+CREATE TABLE public.publishing(
+id uuid NOT NULL,
+caption VARCHAR(255) NOT NULL,
+CONSTRAINT pk_publishing PRIMARY KEY (id)
+);
+
+CREATE TABLE public.genre(
+id UUID NOT NULL,
+caption VARCHAR(255) NOT NULL,
+parent_id UUID,
+CONSTRAINT pk_genre PRIMARY KEY (id),
+CONSTRAINT fk_genre_genre_id FOREIGN KEY (parent_id) REFERENCES genre(id) ON UPDATE CASCADE ON DELETE CASCADE,
+CONSTRAINT uq_genre_caption UNIQUE (caption)
+);
+
+CREATE TABLE public.author(
+id UUID NOT NULL,
+name VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+middlename VARCHAR(255),
+country_id UUID NOT NULL,
+CONSTRAINT pk_id PRIMARY KEY (id),
+CONSTRAINT fk_author_country_id FOREIGN KEY (country_id) REFERENCES country(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
 CREATE TABLE public.comment(
 id UUID NOT NULL,
 title VARCHAR(255) NOT NULL,
@@ -108,7 +108,7 @@ CREATE TYPE agelimit AS ENUM ('0+', '6+', '12+', '16+', '18+');
 
 CREATE TABLE public.book(
 id UUID NOT NULL,
-name VARCHAR(255) NOT NULL,
+caption VARCHAR(255) NOT NULL,
 yearpublication DATE NOT NULL,
 coverpath TEXT,
 publishing_id UUID NOT NULL,
@@ -137,7 +137,7 @@ CONSTRAINT fk_favorite_book_id FOREIGN KEY (book_id) REFERENCES book(id) ON UPDA
 CONSTRAINT uq_favorite_transit UNIQUE (userx_id, book_id)
 );
 
-CREATE TYPE star_value AS ENUM ('0', '0.5', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4,5', '5');
+CREATE TYPE star_value AS ENUM ('0', '0.5', '1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5');
 
 CREATE TABLE public.star(
 id UUID NOT NULL,
@@ -169,7 +169,6 @@ CONSTRAINT fk_comment__book_book_id FOREIGN KEY (book_id) REFERENCES book(id) ON
 CONSTRAINT fk_comment__book_comment_id FOREIGN KEY (comment_id) REFERENCES comment(id) ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT uq_comment__book_transit UNIQUE (book_id, comment_id)
 );
-
 
 CREATE TABLE public.book_on_genre(
 id UUID NOT NULL,
