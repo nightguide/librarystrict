cfb1a5b0b1b3fc6047232c73cadaf86beb90b973
Konstantin <strictkod1899@yandex.ru>
2018-08-30 16:36:19 +0700

    feature (app): implemented an sql-code for create roleuser

---
+++ _sources/sql/2. create data.sql
@@ -0,0 +1,3 @@
+INSERT INTO roleuser (id, code) VALUES ('53103b9d-bfb7-449e-987c-e3981e933edd', 'USER');
+INSERT INTO roleuser (id, code) VALUES ('feddce5f-169d-40f1-a3fb-a792ede41d21', 'EDITOR');
+INSERT INTO roleuser (id, code) VALUES ('df1eda7b-8d78-40c3-a94e-cdd3bbe367ee', 'SUPERUSER');
