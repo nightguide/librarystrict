7a8601498f90c478c35633e93c2c650444021114
Konstantin <strictkod1899@yandex.ru>
2018-08-31 18:05:09 +0700

    featuer (app): implemented a valdiate authentication page

--- app/src/main/java/ru/strict/app/controllers/authentication/AuthenticationController.java
+++ app/src/main/java/ru/strict/app/controllers/authentication/AuthenticationController.java
@@ -4,12 +4,14 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.strict.app.models.authentication.SignInViewModel;
+import ru.strict.app.validations.AuthorizationValidator;
 import ru.strict.components.TokenInfo;
+import ru.strict.components.Url;
 import ru.strict.services.data.requests.RequestAuthUser;
 import ru.strict.services.interfaces.IServiceAuthentication;
 import ru.strict.utils.UtilDate;
@@ -19,12 +21,16 @@ import ru.strict.validates.ValidateBaseValue;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class AuthenticationController {
 
     @Autowired
     private IServiceAuthentication serviceAuthentication;
+
+    @Autowired
+    private AuthorizationValidator authorizationValidator;
 
     @RequestMapping(value="/auth", method=RequestMethod.GET)
     public ModelAndView index(){
@@ -34,18 +40,30 @@ public class AuthenticationController {
     }
 
     @RequestMapping(value="/auth", method=RequestMethod.POST)
-    public ModelAndView signIn(SignInViewModel data){
-        String redirectUrl = "/";
-        ModelAndView model = new ModelAndView();
+    @ResponseBody
+    public Object signIn(@RequestBody SignInViewModel data,
+                         HttpServletResponse response,
+                         BindingResult resultValidation){
+        Object result = null;
 
-        RequestAuthUser request = new RequestAuthUser(data.getUsername(), data.getPassword());
+        authorizationValidator.validate(data, resultValidation);
 
-        if(serviceAuthentication.authUser(request)){
-            redirectUrl = "/books";
+        if(resultValidation.hasErrors()){
+            response.setStatus(400);
+            result = resultValidation.getAllErrors();
+        }else {
+            RequestAuthUser request = new RequestAuthUser(data.getUsername(), data.getPassword());
+            if (serviceAuthentication.authUser(request)) {
+                result = new Url("books");
+            }else{
+                response.setStatus(400);
+                resultValidation.rejectValue("username",
+                        "username.notvalid",
+                        "Неверно введен логин или пароль");
+                result = resultValidation.getAllErrors();
+            }
         }
 
-        model.setViewName("redirect:" + redirectUrl);
-
-        return model;
+        return result;
     }
 }
--- app/src/main/webapp/pages/authentication/index.jsp
+++ app/src/main/webapp/pages/authentication/index.jsp
@@ -6,6 +6,10 @@
         <%@include file="../../resources/css/pre/common_auth_reg.css"%>
         <%@include file="../../resources/css/pre/auth.css"%>
     </style>
+    <script type="text/javascript">
+        <%@include file="../../resources/libs/jquery-3.1.1.min.js"%>
+        <%@include file="../../resources/scripts/core.js"%>
+    </script>
     <title>Авторизация</title>
 </head>
 
@@ -16,7 +20,7 @@
         <img class="logo" src="resources/images/png/Logo_LibraryStrict.png" alt="logo"/>
         <div class="layout_main">
             <p class="title">Авторизация</p>
-            <form class="layout_form" action="\auth" method="post">
+            <form id="auth_form" class="layout_form" action="\auth" method="post">
                 <div class="layout_input_fields">
                     <input type="text" name="username" placeholder="Введите логин или e-mail" class="input_text"/>
                     <p class="error_message"></p>
@@ -26,7 +30,7 @@
                     <p class="error_message"></p>
                 </div>
                 <div class="layout_buttons">
-                    <input type="submit" name="btn_signin" value="войти" class="buttons"/>
+                    <input type="button" id="btn_signin" value="войти" class="buttons"/>
                     <div class="separator"></div>
                     <input type="button" name="btn_reg" value="регистрация" class="buttons"
                             onclick="window.location.href='<%=request.getContextPath()%>/registration'"/>
@@ -34,5 +38,27 @@
             </form>
         </div>
     </div>
+
+    <script>
+        $('#btn_signin').click(function(event){
+            event.preventDefault()
+
+            var formData = convertFormToJSON('#auth_form')
+
+            $.ajax({
+                type: 'POST',
+                url: '/auth',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: JSON.stringify(formData),
+                success: function(data){
+                    window.location.replace(data.url)
+                },
+                error: function(data){
+                    displayErrorsToForm('#auth_form', data.responseJSON);
+                }
+            });
+        });
+    </script>
 </body>
 </html>
